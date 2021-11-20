69850e6fffcd8ea4a4b46b5135b1b62393a9e77d
Roshan Choxi <roshan@popsql.com>
2021-11-19 19:16:15 -0600

    generator script

--- j0ebot.rb
+++ j0ebot.rb
@@ -4,20 +4,20 @@ require "pry"
 
 class Data
   NUMBERS_TO_NAME = {
-    '+12192994442' => 'Joe Janiczek',
-    '+12245780974' => 'Roshan Choxi',
-    '+18472715832' => 'Matt Carter',
-    '+12247230113' => 'Jonas Owen',
-    '+16308999196' => 'Max Schwanebeck',
+    '+12192994442' => 'Joe',
+    '+12245780974' => 'Roshan',
+    '+18472715832' => 'Matt',
+    '+12247230113' => 'Jonas',
+    '+16308999196' => 'Max',
   }
 
   UUID_TO_NAME = {
-    'f2ac7614-a07f-46ee-8703-cb7ed2293840' => 'Max Schwanebeck',
-    '120e1329-f5cd-4b9f-abe8-a77ff7e3be8a' => 'Matt Carter',
-    'cf24122d-0db2-4d3b-bfd9-635eda38a9e1' => 'Schmitty Schmitt',
-    '120e1329-f5cd-4b9f-abe8-a77ff7e3be8a' => 'Krishnan Warrior',
-    'f06e4fc2-34a3-4820-a706-35d32eceb69e' => 'Jonas Owen',
-    '64fa33dc-a206-4357-afee-d96daaa99354' => 'Roshan Choxi',
+    'f2ac7614-a07f-46ee-8703-cb7ed2293840' => 'Max',
+    '120e1329-f5cd-4b9f-abe8-a77ff7e3be8a' => 'Matt',
+    'cf24122d-0db2-4d3b-bfd9-635eda38a9e1' => 'Schmitty',
+    '120e1329-f5cd-4b9f-abe8-a77ff7e3be8a' => 'Krishnan',
+    'f06e4fc2-34a3-4820-a706-35d32eceb69e' => 'Jonas',
+    '64fa33dc-a206-4357-afee-d96daaa99354' => 'Roshan',
     'ac875429-1624-40c7-925c-731fe43d3cf7' => 'Frank'
   }
 
@@ -30,7 +30,7 @@ class Data
       type = message["type"]
       next unless ["incoming", "outgoing"].include?(type)
 
-      message["source_name"] = 'Schmitty Schmitt' if type == "outgoing"
+      message["source_name"] = 'Schmitty' if type == "outgoing"
       message["source_name"] ||= NUMBERS_TO_NAME[number]
       message["source_name"] ||= UUID_TO_NAME[uuid]
 
@@ -42,12 +42,11 @@ class Data
   end
 end
 
-class J0ebot
-  def run
-  end
+messages = Data.load
+formatted = messages.filter {|m| m && m["source_name"] }.map do |message|
+  log = "#{message["source_name"]}: #{message["body"]}"
+  { prompt: '', completion: log }.to_json
 end
 
-messages = Data.load
-messages.filter {|m| m && m["source_name"] }.each do |message|
-  puts "#{message["source_name"]}: #{message["body"]}"
-end
+
+File.write("group-chat.jsonl", formatted.join("\n"))
